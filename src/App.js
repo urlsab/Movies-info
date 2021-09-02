27abd6967b6e581e18ee785291885a815fa74028
yair sabag <yairsabag213@gmail.com>
2021-09-02 18:17:56 +0300

    split code and add navbar

--- src/App.js
+++ src/App.js
@@ -1,75 +1,16 @@
-import React, { useReducer, useEffect } from "react";
-import { initialState, reducer } from "./reducer/reducer";
+import React from 'react';
 import './App.css';
-import axios from "axios";
 
-import Header from "./components/Header/Header";
-import Movie from "./components/Movie/Movie";
-import Search from "./components/SearchFunc/SearchFunc";
-import spinner from "./assets/spinner.gif";
-
-// const MY_API_KEY = process.env.REACT_APP_MOVIES_API_KEY;
-const MOVIE_API_URL = `https://www.omdbapi.com/?s=man&apikey=c9f4232`;
+import RetrievedMovies from "./components/RetrivedMovies/RetrivedMovies";
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  // The first auto result as we enter the website
-  useEffect(() => {
-    axios.get(MOVIE_API_URL).then(jsonResponse => {
-      dispatch({
-        type: "SEARCH_MOVIES_SUCCESS",
-        payload: jsonResponse.data.Search
-      });
-    });
-  }, []);
-
-  // result for any search
-  const search = searchValue => {
-    dispatch({
-      type: "SEARCH_MOVIES_REQUEST"
-    });
-
-    axios(`https://www.omdbapi.com/?s=${searchValue}&apikey=c9f4232`).then(
-      jsonResponse => {
-        if (jsonResponse.data.Response === "True") {
-          dispatch({
-            type: "SEARCH_MOVIES_SUCCESS",
-            payload: jsonResponse.data.Search
-          });
-        } else {
-          dispatch({
-            type: "SEARCH_MOVIES_FAILURE",
-            error: jsonResponse.data.Error
-          });
-        }
-      }
-    );
-  };
-
-  // include the properties from the reducer object
-  const { movies, errorMessage, loading } = state;
-
-  // render the movies with some conditons
-  const retrievedMovies =
-    loading && !errorMessage ? (
-      <img className="spinner" src={spinner} alt="Loading spinner" />
-    ) : errorMessage ? (
-      <div className="errorMessage">{errorMessage}</div>
-    ) : (
-      movies.map((movie, index) => (
-        <Movie key={`${index}-${movie.Title}`} movie={movie} />
-        // mayby add an <Forbidden /> component for movies that movie.rated === "PG-13" e.g - will map different components
-      ))
-    );
-
+  
   return (
     <div className="App">
       <div className="m-container">
-        <Header text="MOVIES INFO"/>
-        <Search search={search} />
-        <p className="App-intro">Sharing a few of our favourite movies</p>
-        <div className="movies">{retrievedMovies}</div>
+        <Navbar />
+        <RetrievedMovies />
       </div>
     </div>
   );
---
+++ src/components/RetrivedMovies/RetrivedMovies.js
@@ -0,0 +1,79 @@
+import React, { useReducer } from "react";
+import { initialState, reducer } from "../../reducer/reducer";
+import './RetrivedMovies.css';
+import axios from 'axios';
+
+import Movie from '../Movie/Movie';
+import SearchFunc from '../SearchFunc/SearchFunc';
+import spinner from '../../assets/spinner.gif'; 
+
+const MY_API_KEY = process.env.REACT_APP_MOVIES_API_KEY;
+let checkEntry = 0;
+
+const RetrievedMovies = () => {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+  // result for any search
+  const getSearch = searchValue => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
+
+    // just 'axios' is === to 'axios.get'
+    axios(`https://www.omdbapi.com/?s=${searchValue}&apikey=${MY_API_KEY}`).then(
+      jsonResponse => {
+        if (jsonResponse.data.Response === "True") {
+          checkEntry++;
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.data.Search
+          });
+        } else {
+          dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.data.Error
+          });
+        }
+      }
+    );
+  };
+
+  // include the properties from the reducer object
+  const { movies, errorMessage, loading } = state;
+  
+
+  // render the movies with some conditionals
+  const showMovies =
+
+    loading && checkEntry !== 0 && !errorMessage ? (
+      <img className="spinner" src={spinner} alt="Loading spinner" />
+    ) : 
+
+      // if user never searched
+      loading && checkEntry === 0 && !errorMessage ? (
+      <p>type any word</p>
+    ):
+
+      errorMessage ? (
+      <div className="errorMessage">{errorMessage}</div>
+    ) : 
+    
+    (
+      movies.map((movie, index) => (
+        <Movie key={`${index}-${movie.Title}`} movie={movie} />
+        // mayby add an <Forbidden /> component for movies that movie.rated === "PG-13" e.g - will map different components
+      )
+    )
+  );
+
+  return (
+    <div>
+      <SearchFunc search={getSearch} />
+      <div className="movies"> {showMovies} </div>
+    </div>
+  );
+
+}
+
+export default RetrievedMovies;
